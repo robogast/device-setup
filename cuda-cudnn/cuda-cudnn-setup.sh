c94575bf78d2003da39fd21cf58aeee19a741d8a
Robert Jan Schlimbach <robertjan.schlimbach@gmail.com>
2020-05-20 12:05:40 +0200

    some more boilerplate for cuda

--- cuda-cudnn/cuda-cudnn-setup.sh
+++ cuda-cudnn/cuda-cudnn-setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sudo apt install linux-headers-$(uname -r)
+sudo apt install linux-headers-$(uname -r) build-essential
 
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
 sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
-wget http://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
+wget -nc http://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
 sudo dpkg -i cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
 sudo apt-key add /var/cuda-repo-10-2-local-10.2.89-440.33.01/7fa2af80.pub
 sudo apt update
@@ -13,4 +13,10 @@ sudo apt install -y cuda
 export PATH=/usr/local/cuda-10.2/bin:/usr/local/cuda-10.2/NsightCompute-2019.1${PATH:+:${PATH}}
 export LD_LIBRARY_PATH=/usr/local/cuda-10.2/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 
-sudo dpkg -i libcudnn7_7.6.5.32-1+cuda10.2_amd64.deb
+$CUDNN_FILE="libcudnn7_7.6.5.32-1+cuda10.2_amd64.deb"
+if [ ! -f "$CUDNN_FILE" ]; then
+    sudo apt install git-lfs
+    git lfs pull
+fi
+
+sudo dpkg -i $CUDNN_FILE
